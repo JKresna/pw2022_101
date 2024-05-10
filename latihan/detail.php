7ab207c73f0821599ed3050677c5e93903f63cb7
JKresna <darktraveller01@gmail.com>
2024-05-10 17:15:31 +0700

    Mengblock jika belum login

--- latihan/detail.php
+++ latihan/detail.php
@@ -1,51 +1,58 @@
 <?php 
-require "functions.php";
+session_start();
 
-if (!isset($_GET["id"])) {
-  header("Location: index.php");
+if (!isset($_SESSION["login"])) {
+  header("Location: login.php");
   exit;
 }
 
-// Ambil mahasiswa dengan id yang ada di URL
+require "function.php";
+
+// Jika parameter id tidak ada
+if (!isset($_GET["id"])) {
+	header("Location: index.php");
+	exit;
+}
+
 $id = mysqli_real_escape_string(koneksi(), $_GET["id"]);
-$mhs = query("SELECT * FROM mahasiswa WHERE id='$id'");
-?>
+$mahasiswa = query("SELECT * FROM mahasiswa WHERE id='$id'");
+
+?>		
 <!DOCTYPE html>
 <html lang="id">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Detail Mahasiswa</title>
-  <style>
-    img {
-	width: 100px;
-	height: 100px;
-    }
-
-    ul li a {
-	text-decoration: none;
-    }
-  </style>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Detail Mahasiswa</title>
+	<style>
+		body {font-family: arial, sans-serif;}
+		
+		img {
+			width: 100px;
+			height: 100px;
+		}
+	</style>
 </head>
 <body>
 
-  <h3>Detail Mahasiswa</h3>
+	<h3>Detail Mahasiswa</h3>
 
-  <ul>
-    <li>
-      <img src="img/<?= htmlspecialchars($mhs['gambar']); ?>" alt="Profil">
-    </li>
-    <li>Nama : <?= htmlspecialchars($mhs['nama']); ?></li>
-    <li>NRP : <?= htmlspecialchars($mhs['nrp']); ?></li>
-    <li>Jurusan : <?= htmlspecialchars($mhs['jurusan']); ?></li>
-    <li>Email : <?= htmlspecialchars($mhs['email']); ?></li>
-    <li>
-      <a href="ubah.php?id=<?= $mhs['id'];?>">Ubah</a> | <a href="hapus.php?id=<?= $mhs['id'];?>" onclick="return confirm('Anda Yakin?');">Hapus</a>
-    </li>
-  </ul>
+	<ul>
+		<li>
+			<img src="img/<?= htmlspecialchars($mahasiswa['gambar']); ?>" alt="Gambar <?= htmlspecialchars($mahasiswa['nama']); ?>">
+		</li>
+		<li>Nama : <?= htmlspecialchars($mahasiswa["nama"]); ?></li>
+		<li>NRP : <?= htmlspecialchars($mahasiswa["nrp"]); ?></li>
+		<li>Email : <?= htmlspecialchars($mahasiswa["email"]); ?></li>
+		<li>Jurusan : <?= htmlspecialchars($mahasiswa["jurusan"]); ?></li>
+		<li>
+			<a href="ubah.php?id=<?= $mahasiswa['id']; ?>">Ubah</a> | <a href="hapus.php?id=<?= $mahasiswa['id']; ?>" onclick="return confirm('Anda Yakin?');">Hapus</a>
+		</li>
+	</ul>
 
-  <br><br>
-  <a href="index.php">Kembali ke daftar mahasiswa</a>
+	<br><br>
+
+	<a href="index.php">Kembali ke Daftar Mahasiswa</a>
 
 </body>
 </html>
